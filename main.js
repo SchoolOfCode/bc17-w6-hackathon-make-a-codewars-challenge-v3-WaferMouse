3c634247bc85583ced92b6ff596371e023190f10
Jocelyne Jones <gothling@jocely.net>
2024-08-09 10:52:43 +0100

    wrote a better solution and deleted it

--- main.js
+++ main.js
@@ -2,6 +2,6 @@ export function treeFinder(buriedTree) {
 
   let foundTree = buriedTree //replace this line with your solution
 
-  console.log(foundTree)
-  return(foundTree)
+  console.log(foundTree);
+  return(foundTree);
 }
