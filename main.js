ae8a8f1d53aa4cbe9d9ff947d89094597fbd6acf
Jocelyne Jones <gothling@jocely.net>
2024-08-09 11:23:06 +0100

    added instructions to comment in main.js

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
+/*
+Christmas will be here before you know it, but you don't have a Christmas tree! Fortunately there's one hidden in this function. Unfortunately it got turned into numbers and buried in letters. It should look like this:
+
+    #    
+   ###   
+  #####  
+ ####### 
+#########
+    #    
+  #####  
+   ###   
+
+The test will pass the following text to the function, try to pull the tree out of it, and make sure that it's made out of `#`s.
+
+dksh1sadj
+skl398alo
+fg10749gh
+b2047374v
+339206120
+folw4ldrw
+qw10934lm
+lwd563fdm
+*/
+
 export function treeFinder(buriedTree) {
 
   let foundTree = buriedTree //replace this line with your solution
